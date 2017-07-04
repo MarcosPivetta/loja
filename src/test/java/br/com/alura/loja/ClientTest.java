323a84b67adb7620f7e90b887f82656d4ae18924
Marcos Pivetta <m.pivetta>
2017-07-04 14:17:52 -0300

    Aula 6 -  O protocolo HTTP por baixo dos panos

--- src/test/java/br/com/alura/loja/ClientTest.java
+++ src/test/java/br/com/alura/loja/ClientTest.java
@@ -2,7 +2,10 @@ package br.com.alura.loja;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -12,6 +15,7 @@ import org.junit.Test;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 import junit.framework.Assert;
 
 public class ClientTest {
@@ -29,15 +33,33 @@ public class ClientTest {
 	}
 
 	@Test
-	public void tetaQueBuscaUmCarrinhoTrazOCarrinhoEsperado() {
+	public void testaQueBuscaUmCarrinhoTrazOCarrinhoEsperado() {
 
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8081");
+		
 		String conteudo = target.path("/carrinhos/1").request().get(String.class);
 		System.out.println(conteudo);
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+		
+	}
+	
+	@Test
+	public void testaInclusaoDoCarrinho() {
 
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8081");
+		
+		Carrinho carrinho2 = new Carrinho();
+        carrinho2.adiciona(new Produto(314L, "Tablet", 999, 1));
+        carrinho2.setRua("Rua Vergueiro");
+        carrinho2.setCidade("Sao Paulo");
+        String xml = carrinho2.toXML();
+
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+        Response response = target.path("/carrinhos").request().post(entity);
+        Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
 	}
 
 }
